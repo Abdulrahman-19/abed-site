32c1e81a90991e6cab5944cf9af30645369eae4a
Abdulrahman-19 <abedabogneem@gmail.com>
2021-03-08 12:16:00 +0200

    css

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function alertFunction(){
+    alert('welcome');
+}
+alertFunction();
+
+var site = function(){
+for(var i=0; i < 3 ; i++){
+    document.write('abed Site' + '<br>');
+    }
+}
+site();
